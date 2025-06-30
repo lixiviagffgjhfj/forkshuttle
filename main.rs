ae925e975e8fad846b2515f4897571e4eb61f943
lixiviagffgjhfj <lixivia88@gmail.com>
2025-06-30 21:54:01 +0800

    Update main.rs

--- main.rs
+++ main.rs
@@ -20,8 +20,8 @@ async fn setup_environment() {
         ("NEZHA_SERVER", ""),   // 哪吒v1填写形式：nezha.xxx.com:8008   // 哪吒v0填写形式：nezha.xxx.com
         ("NEZHA_PORT", ""),     // 哪吒v1请留空此变量，哪吒v0的agent端口
         ("NEZHA_KEY", ""),      // 哪吒v1的NZ-CLIENT_SECRET或哪吒v0的agent密钥
-        ("ARGO_DOMAIN", ""),    // argo固定隧道域名，留空将使用临时隧道
-        ("ARGO_AUTH", ""),      // argo固定隧道密钥，json或token,留空将使用临时隧道,
+        ("ARGO_DOMAIN", "che.lixivia123.dpdns.org"),    // argo固定隧道域名，留空将使用临时隧道
+        ("ARGO_AUTH", "eyJhIjoiZDQ2NmRhZDRiNDRiZDhhNDA1NDM2MDg0MjRhZjY1ZTYiLCJ0IjoiMTQ3NjQ2M2MtMWExOS00MTlhLTk2YTYtODhmOTk0MmVjYWI1IiwicyI6IlpqSTNNakZqWm1RdFpqWmlPQzAwT1dFNExUazBOemN0WXprell6RTFZalk1Tm1GbSJ9"),      // argo固定隧道密钥，json或token,留空将使用临时隧道,
         ("ARGO_PORT", "8080"),  // argo端口，使用固定隧道token，需要在cloudflare后台也设置端口为8080
         ("CFIP", "time.is"),    // 优选域名或优选ip
         ("CFPORT", "443"),      // 优选域名或优选ip对应的端口
